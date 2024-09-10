bdc6362038c17a4b3555758fc4fd9d877c826721
Abdulafeez Adetunmbi <xplicifire@gmail.com>
2024-09-10 19:45:31 +0100

    init

---
+++ src/pages/Register.tsx
@@ -0,0 +1,86 @@
+import { useForm } from "react-hook-form"
+import { useMutation, useQueryClient } from "react-query"
+import * as apiClient from '../api-client'
+import { useAppContext } from "../contexts/AppContext"
+import { Link, useNavigate } from "react-router-dom"
+
+export type RegisterFormData = {
+    firstName : string,
+    lastName : string,
+    email : string,
+    password : string,
+    confirmPassword : string
+}
+
+const Register = () => {
+
+    const queryClient = useQueryClient()
+
+    const {showToast} = useAppContext()
+
+    const navigate = useNavigate()
+
+    const {register, watch, handleSubmit, formState : {errors}} = useForm<RegisterFormData>()
+
+    const mutation = useMutation(apiClient.register, {
+
+        onSuccess : async () => {
+            showToast({message: 'Registration Success!',type : 'SUCCESS'})
+            await queryClient.invalidateQueries('validateToken')
+            navigate('/')
+        },
+        onError : (error : Error) => {
+            showToast({message: error.message,type : 'ERROR'})
+        }
+        
+    })
+
+    const onSubmit = handleSubmit((data) => {
+          
+        mutation.mutate(data)
+    })
+
+  return (
+    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
+        <h2 className="text-3xl font-bold">Create an Account</h2>
+        <div className="flex flex-col md:flex-row gap-5">
+            <label className="text-gray-700 text-sm font-bold flex-1">First Name
+                  <input className="border border-blue-800 rounded w-full py-1 px-2 font-normal" {...register("firstName", {required : "This field is required"})}/>
+                  {errors.firstName && <span className="text-red-500">{errors.firstName.message}</span>}
+            </label>
+            <label className="text-gray-700 text-sm font-bold flex-1">Last Name
+                  <input className="border border-blue-800  rounded w-full py-1 px-2 font-normal" {...register("lastName", {required : "This field is required"})}/>
+                  {errors.lastName && <span className="text-red-500">{errors.lastName.message}</span>}
+                  
+            </label>
+        </div>
+        <label className="text-gray-700 text-sm font-bold flex-1">Email
+                  <input type="email" className="border border-blue-800  rounded w-full py-1 px-2 font-normal" {...register("email", {required : "This field is required"})}/>
+                  {errors.email && <span className="text-red-500">{errors.email.message}</span>}
+        </label>
+        <label className="text-gray-700 text-sm font-bold flex-1">Password
+                  <input type="password" minLength={6} className="border border-blue-800  rounded w-full py-1 px-2 font-normal" {...register("password", {required : "This field is required"})}/>
+                  {errors.password && <span className="text-red-500">{errors.password.message}</span>}
+        </label>
+        <label className="text-gray-700 text-sm font-bold flex-1">Confirm Password
+                  <input type="password" minLength={6} className="border border-blue-800  rounded w-full py-1 px-2 font-normal" {...register("confirmPassword", {
+                    validate : (val) => {
+                      if(!val) return 'This field is required';
+
+                      if(watch("password") !== val) return "Passwords do not match"
+
+                    }
+                  })}/>
+                  {errors.confirmPassword && <span className="text-red-500">{errors.confirmPassword.message}</span>}
+        </label>
+        <span className='flex items-center justify-between'>
+        <span className='text-sm'>Already Registered? <Link to='/sign-in' className='underline'>Sign In here</Link></span>
+            <button type="submit" className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl">
+                Create Account
+            </button>
+        </span>
+    </form>
+  )
+}
+
+export default Register
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+    container : {
+      padding:{
+        md : "10rem"
+      } 
+    }
+  },
+  plugins: [],
+}
+
